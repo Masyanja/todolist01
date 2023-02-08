4bb94a9ed179dc8af9f19cb2f85f45afdab6ce8c
Masyanja <107952755+Masyanja@users.noreply.github.com>
2023-02-08 14:48:15 +0300

    Add files via upload

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,22 +1,10 @@
-"""todolist URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
 
+from todolist.core.views import LoginView, ProfileView, SignupView, UpdatePasswordView
+
 urlpatterns = [
-    path('core/', include('todolist.core.urls')),
-    path('admin/', admin.site.urls),
+    path('signup', SignupView.as_view(), name='signup'),
+    path('login', LoginView.as_view(), name='login'),
+    path('profile', ProfileView.as_view(), name='profile'),
+    path('update_password', UpdatePasswordView.as_view(), name='update-password'),
 ]
